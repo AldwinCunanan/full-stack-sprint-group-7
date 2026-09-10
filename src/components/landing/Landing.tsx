d58a7e7ac4040f62d7482eb8ed370f508c73e0a7
August <mturzak@rrc.ca>
2026-09-10 14:25:13 -0500

    Added more racers to the list

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,6 +1,26 @@
 
 const racers = [
     {
+        name: "Luna Vortex",
+        team: "Nebula Racing",
+        number: 44,
+        wins: 10,
+        ranking: "1st Overall",
+        odds: "+150",
+        vehicle: "Nebula NX44",
+        color: "Purple & Black"
+    },
+    {
+        name: "Orion Blaze",
+        team: "Starlance Racing",
+        number: 31,
+        wins: 4,
+        ranking: "2nd Overall",
+        odds: "+200",
+        vehicle: "Starlance SB31",
+        color: "Blue & Silver"
+    },
+    {
         name: "Max Velocity",
         team: "Thunderbolt Racing",
         number: 22,
@@ -11,14 +31,14 @@ const racers = [
         color: "Matte Red & Black"
     },
     {
-        name: "Apex Strider",
-        team: "Crimson Apex",
-        number: 14,
-        wins: 3,
-        ranking: "8th Overall",
-        odds: "+600",
-        vehicle: "Apex R14",
-        color: "Crimson & Silver"
+        name: "Vega Storm",
+        team: "Cyclone Dynamics",
+        number: 18,
+        wins: 6,
+        ranking: "4th Overall",
+        odds: "+300",
+        vehicle: "Cyclone CX18",
+        color: "Teal & Black"
     },
     {
         name: "Nova Drift",
@@ -31,24 +51,54 @@ const racers = [
         color: "Yellow & Black"
     },
     {
+        name: "Crimson Fang",
+        team: "Redline Syndicate",
+        number: 11,
+        wins: 2,
+        ranking: "6th Overall",
+        odds: "+500",
+        vehicle: "Redline RL11",
+        color: "Blood Red"
+    },
+    {
+        name: "Titan Flux",
+        team: "Ironclad Motors",
+        number: 5,
+        wins: 0,
+        ranking: "7th Overall",
+        odds: "+550",
+        vehicle: "Ironclad IC5",
+        color: "Gunmetal Grey"
+    },
+    {
+        name: "Apex Strider",
+        team: "Crimson Apex",
+        number: 14,
+        wins: 3,
+        ranking: "8th Overall",
+        odds: "+600",
+        vehicle: "Apex R14",
+        color: "Crimson & Silver"
+    },
+    {
+        name: "Echo Mirage",
+        team: "Phantom Circuit",
+        number: 27,
+        wins: 1,
+        ranking: "9th Overall",
+        odds: "+700",
+        vehicle: "Phantom PH27",
+        color: "Ghost White"
+    },
+    {
         name: "Rex Tempest",
         team: "Tempest Motors",
         number: 9,
         wins: 1,
-        ranking: "12th Overall",
+        ranking: "10th Overall",
         odds: "+1200",
         vehicle: "Tempest T9",
         color: "Storm Grey"
-    },
-    {
-        name: "Luna Vortex",
-        team: "Nebula Racing",
-        number: 44,
-        wins: 10,
-        ranking: "1st Overall",
-        odds: "+150",
-        vehicle: "Nebula NX44",
-        color: "Purple & Black"
     }
 ];
 
