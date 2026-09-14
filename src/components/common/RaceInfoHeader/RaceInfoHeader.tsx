35387cc367254147fc73802bbceb093d91537fa6
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-09-14 16:31:09 -0500

    feat: Added RaceInfoHeader component to display race track image and page title. feat: Added Montreal F1 track png.

---
+++ src/components/common/RaceInfoHeader/RaceInfoHeader.tsx
@@ -0,0 +1,14 @@
+import track_canada_montreal from "../../../assets/track_canada_montreal.png"
+
+function RaceInfoHeader(){
+    return(
+        <div>
+            <header className="race-info-header">
+                <h1>Race Information</h1>
+                <img className="race-track-image" src={track_canada_montreal} alt="race track image" />
+            </header>
+        </div>
+    )
+}
+
+export default RaceInfoHeader;
