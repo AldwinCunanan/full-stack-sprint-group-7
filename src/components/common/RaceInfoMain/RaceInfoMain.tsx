a959ae90ee6277eb345d91aafc612003c43a8051
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-09-14 17:33:18 -0500

    feat: Added RaceInfoMain component which displays race information such as, race name, location and track length.

---
+++ src/components/common/RaceInfoMain/RaceInfoMain.tsx
@@ -0,0 +1,43 @@
+
+const race = {
+    name: "Canadian Gradn Prix",
+    location: "Montreal Canada",
+    trackLength: "4.6km",
+    racers: [
+        "Max Ver",
+        "Lar Aper",
+        "Hunter Bow",
+    ]
+};
+
+function RaceInfoMain(){
+    return(
+        <main>
+            <div className="race-info-main">
+                <div className="race-image">
+                    <div className="race-info">
+                        <div>
+                            <span>Race Name</span>
+                            <p className="race-name"></p>
+                        </div>
+                        
+                        <div>
+                            <span>Location</span>
+                            <p className="race-location"></p>
+                        </div>
+                        
+                        <div>
+                            <span>Track Length</span>
+                            <p className="track-length"></p>
+                        </div>
+
+                        
+
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default RaceInfoMain;
