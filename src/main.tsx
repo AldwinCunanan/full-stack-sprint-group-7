0e58515689f5168c7a856ff824c9b851d0cd244e
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-09-15 18:40:52 -0500

    trigger vercel.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./main.css";
-import "./raceInfo.css";
 
 // creates the Root React component of our app at the #root HTML element
 createRoot(document.getElementById('root')!).render(
