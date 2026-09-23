2aa8accf4ab2264b66dd0e5b79eacb620c86da18
August <mturzak@academic.rrc.ca>
2026-09-22 21:50:32 -0500

    updated main.tsx to support react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import "./main.css";
 
-// creates the Root React component of our app at the #root HTML element
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* renders the App component */}
-    <App />
+    <BrowserRouter>
+      {/* renders the App component */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
-
-/**
- * <StrictMode> <App /> </StrictMode> are React Nodes. 
-* The .tsx file type ("TypeScript XML") allows these nodes to be written in TypeScript with HTML-like syntax
- */
